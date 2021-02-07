afdca4d5ffc3ee8e00ce9a07fca4f64501667ed6
Winnie.Chen <37991693+cyx441984694@users.noreply.github.com>
2021-02-07 20:45:28 +0800

    Add files via upload

---
+++ springcloudproducer/src/main/java/com.neo/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.neo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    /// Performing gateway testing
+//    @RequestMapping("/hello")
+//    public String index() {
+//        return "hello world!";
+//    }
+
+    @RequestMapping("/foo")
+    public String foo(String foo) {
+        return "hello "+foo+"!";
+    }
+
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name){
+        return "hello  " + name +", this is first massage";
+    }
+}
